005c2aa86026a570c687c66425f8edc2e6cda72b
Nastya <nastasya-ignatova@mail.ru>
2021-06-17 14:13:02 +0300

    Первый коммит

---
+++ js/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+a = 5;
+console.log(a); 
+
+// var number = 5;
+// var string = "hi";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4/0);
+// console.log('gogo'*9);
+// let some;
+// console.log(some);
+
+// let person = {
+//     name: "holy",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(person["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[2]);
+
+//alert ("hello");
+// let answer = confirm("hello?");
+// console.log(answer);
+//  let answer = +prompt("how old are you?", "yes");
+//  console.log(typeof(answer));
+
+//  console.log("arr" + "-object");
+//  console.log(4 + + "-object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = true;
+
+console.log(isChecked || !isClose);
+
+
+
+
+
